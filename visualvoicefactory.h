e4de8de1c903c263c16bf035d08d8d1b5ed02c06
Greedysky <greedysky@163.com>
2026-03-03 11:06:48 +0800

    style: Code format clean

--- visualvoicefactory.h
+++ visualvoicefactory.h
@@ -30,7 +30,7 @@
 /*!
  * @author Greedysky <greedysky@163.com>
  */
-class VisualVoiceFactory : public QObject, public VisualFactory
+class VisualVoiceFactory : public QObject, private VisualFactory
 {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID "org.qmmp.qmmp.VisualFactoryInterface.1.0")
